b49086e0d5d5f11cf4029d2a80b6717188521158
Luka <luka.talevski.hn@gmail.com>
2025-03-13 14:34:39 +0100

    Remove useless map struct

--- src/util.h
+++ src/util.h
@@ -16,18 +16,6 @@ void *xmalloc(size_t len);
 
 char *progname(char *name, char *fallback);
 
-struct map {
-        size_t len, cap;
-        struct mapkey *keys;
-        void **vals;
-};
 
-struct mapkey {
-        unsigned long hash;
-        const void *str;
-        size_t len;
-};
-
-void **mapput(struct map *map, struct mapkey *key);
-void mapkey(struct mapkey *key, const void *string, size_t len);
-void mapinit(struct map *map, size_t cap);
+bool isDigit(char c);
+bool isAlpha(char c);
